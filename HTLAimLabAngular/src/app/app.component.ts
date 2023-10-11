d67e103c67fcf61cf225fadc18779b0951a7c204
FreezyCGI <patrick.fritzl@gmx.at>
2023-10-11 14:41:18 +0200

    Sample page

--- HTLAimLabAngular/src/app/app.component.ts
+++ HTLAimLabAngular/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'HTLAimLabAngular';
+  title = "HTL Aim Lab";
+
+  ChangeTitle(title:string){
+    this.title = title;
+  }
 }
